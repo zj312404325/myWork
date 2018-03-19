96928b7d8a28dc4b5cbb21389a21ab810e7061b0
eric zhang <312404325@qq.com>
2018-03-19 17:06:36 +0800

    update

--- app/src/main/java/com/example/administrator/jymall/IndexActivity.java
+++ app/src/main/java/com/example/administrator/jymall/IndexActivity.java
@@ -159,8 +159,7 @@ public class IndexActivity extends TopSearchActivity  {
         progressDialog.show();
         Map<String, String> maps= new HashMap<String, String>();
         maps.put("serverKey", super.serverKey);
-        maps.put("id", "2C9383F162037598016204E2CDEB001C");
-        //maps.put("id", "FF8080816227469701622CCE6EAB002F");
+        maps.put("id", CommonUtil.getStrings(R.string.ad_id));
         XUtilsHelper.getInstance().post("app/indexAdData.htm", maps,new XUtilsHelper.XCallBack(){
 
             @SuppressLint("NewApi")
